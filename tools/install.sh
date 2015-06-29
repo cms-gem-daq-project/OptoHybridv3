9f6350893884142abba3ac2beea4f423ea89d00e
Thomas Lenzi <tlenzi@ulb.ac.be>
2015-06-29 10:20:09 +0200

    Small changes to directory structure and scripts

--- tools/install.sh
+++ tools/install.sh
@@ -7,15 +7,25 @@ echo "-- firmware/software"
 echo "--------------------------------------------------------"
 echo ""
 
+# Go to root directory of repository
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+
 # GBT-FPGA
 
-if [ -d "vendors/gbt-fpga" ]; then
-    echo "GBT-FPGA is present, skipping..."
+if [ -d "${DIR}/vendors/gbt-fpga" ]; then
+    echo "GBT-FPGA is present, updating..."
+    cd ${DIR}/vendors/gbt-fpga/tags
+    svn up
+    cd ${DIR}
 else
     echo "GBT-FPGA not found, checking out the source..."
     mkdir -p vendors/gbt-fpga
-    cd vendors/gbt-fpga
+    cd ${DIR}/vendors/gbt-fpga
     svn co https://svn.cern.ch/reps/ph-ese/be/gbt_fpga/tags
+    cd ${DIR}
 fi
+
+#
 
 echo ""
