eff65121a89f07e62c382549eb5abd8be8cf6450
Andrew Peck <andrew.peck@cern.ch>
2020-07-21 23:04:45 -0400

    Add TMR option to the counter block

--- src/utils/counter.vhd
+++ src/utils/counter.vhd
@@ -18,6 +18,7 @@ use ieee.numeric_std.all;
 
 entity counter is
   generic(
+    g_EN_TMR         : integer := 1;
     g_COUNTER_WIDTH  : integer := 32;
     g_ALLOW_ROLLOVER : boolean := false;
     g_INCREMENT_STEP : integer := 1
@@ -33,23 +34,36 @@ end counter;
 architecture counter_arch of counter is
 
   constant max_count : unsigned(g_COUNTER_WIDTH - 1 downto 0) := (others => '1');
-  signal count       : unsigned(g_COUNTER_WIDTH - 1 downto 0);
+
+  type count_array_t is array(integer range 2*g_EN_TMR downto 0) of unsigned (g_COUNTER_WIDTH - 1 downto 0);
+  signal count : count_array_t;
+
+  function majority (a : unsigned; b : unsigned; c : unsigned)
+    return unsigned is
+    variable tmp : unsigned (a'length-1 downto 0);
+  begin
+    tmp := (a and b) or (b and c) or (a and c);
+    return tmp;
+  end function;
 
 begin
 
-  process(ref_clk_i)
+  tmr_loop : for I in 0 to 2*g_EN_TMR generate
   begin
-    if rising_edge(ref_clk_i) then
-      if reset_i = '1' then
-        count_o <= (others => '0');
-        count   <= (others => '0');
-      else
-        if en_i = '1' and (count < max_count or g_ALLOW_ROLLOVER) then
-          count <= count + g_INCREMENT_STEP;
+    process(ref_clk_i)
+    begin
+      if rising_edge(ref_clk_i) then
+        if reset_i = '1' then
+          count(I) <= (others => '0');
+        else
+          if en_i = '1' and (count(I) < max_count or g_ALLOW_ROLLOVER) then
+            count(I) <= count(I) + g_INCREMENT_STEP;
+          end if;
         end if;
-        count_o <= std_logic_vector(count);
       end if;
-    end if;
-  end process;
+    end process;
+  end generate;
+
+  count_o <= std_logic_vector(majority(count(0),count(1),count(2)));
 
 end counter_arch;
